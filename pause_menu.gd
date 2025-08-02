bcfe61e711aceb34322f212da884a33c8eb824d8
mzolfo <32077843+mzolfo@users.noreply.github.com>
2025-08-02 18:43:59 -0500

    made a pause menu

---
+++ pause_menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+func _ready() -> void:
+	 # Replace with function body.
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+
+
+func _on_resume_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Menus/main_menu.tscn")
+	pass # Replace with function body.
